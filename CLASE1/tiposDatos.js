57c2d42fb514ad2131686a605894e5407fda23fe
Edgar <Ed@gmail.com>
2026-02-20 23:18:32 -0600

    Update typeDatos.js

---
+++ CLASE1/tiposDatos.js
@@ -0,0 +1,9 @@
+console.log(typeof "Hola Comunidad DEVF")
+console.log(typeof 42);
+console.log(typeof true);
+console.log(typeof undefined);
+console.log(typeof null);
+console.log(typeof 0.5);
+console.log(typeof "Veinticinco");
+console.log(typeof false);
+

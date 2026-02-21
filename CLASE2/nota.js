843d19ba7d1b26b6948bac1045c1725cafd30805
Edgar <Ed@gmail.com>
2026-02-20 23:41:54 -0600

    Creacion del archivo nota

---
+++ CLASE2/nota.js
@@ -0,0 +1,11 @@
+let nota=92;
+
+if (nota>=90) {
+    console.log("Alumno de excelencia");
+} else if (nota>=75 && nota<=89) {
+    console.log("Alumno bien");
+}   else if (nota>=60 && nota<=74) {
+    console.log("Alumno regular");
+} else if (nota<60) {
+    console.log("No aprobado");
+}

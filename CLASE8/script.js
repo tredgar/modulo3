b46399bdff3c34f3911ff1738e48d53a39b0fc25
Edgar <Ed@gmail.com>
2026-02-28 00:25:40 -0600

    Actualización de estilos y uso de local storage

--- CLASE8/script.js
+++ CLASE8/script.js
@@ -1,13 +1,14 @@
-console.log('Hola, mundo!');
+console.log("Hola, mundo!");
 const form = document.querySelector("form");
-const  nameInput = document.getElementById('name');
-const  commentInput = document.getElementById('comment');
+const nameInput = document.getElementById("name");
+const commentInput = document.getElementById("comment");
 const commentsList = document.getElementById("comments");
 
 let comentarios = [];
+const dateHoure = new Date();
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault(); 
+  event.preventDefault();
 
   const name = nameInput.value;
   const comment = commentInput.value;
@@ -15,8 +16,8 @@ form.addEventListener("submit", (event) => {
   if (name === "" || comment === "") return;
 
   comentarios.push({ name, comment });
-
-
+    const comentarios_json = JSON.stringify(comentarios);
+    localStorage.setItem("comentarios", comentarios_json);
   nameInput.value = "";
   commentInput.value = "";
 
@@ -24,33 +25,39 @@ form.addEventListener("submit", (event) => {
 });
 
 const renderComments = (comments) => {
-    commentsList.innerHTML = '';
-    for (let i = 0; i < comments.length; i++) {
-        const commentCard = document.createElement('div');
-        commentCard.classList.add('comment-card');
+  commentsList.innerHTML = "";
+  for (let i = 0; i < comments.length; i++) {
+        const commentCard = document.createElement("div");
+        commentCard.classList.add("comment-card");
 
-        const commentName = document.createElement('h3');
+        const commentName = document.createElement("h3");
         commentName.textContent = comments[i].name;
-          
-        const date = document.createElement("p");
-         commentCard.classList.add("date");
 
-        const commentText = document.createElement('p');
+        const date = document.createElement("p");
+        date.classList.add("date");
+        date.textContent = "Publicado el " + dateHoure.toLocaleString("es-ES");
+        const commentText = document.createElement("p");
         commentText.textContent = comments[i].comment;
 
-        const deleteButton = document.createElement('button');
-        deleteButton.textContent = 'Borrar';
-        deleteButton.addEventListener('click', () => {
-            comentarios.splice(i, 1);
-            renderComments(comentarios);
-        });
+        const deleteButton = document.createElement("button");
+        deleteButton.classList.add("btn-delete");
+        deleteButton.textContent = "Borrar";
+        deleteButton.addEventListener("click", () => {
+        comentarios.splice(i, 1);
+        renderComments(comentarios);
+    });
 
-        commentCard.appendChild(commentName);
-        commentCard.appendChild(commentText);
-        commentCard.appendChild(deleteButton);
+    commentCard.appendChild(commentName);
+    commentCard.appendChild(date);
+    commentCard.appendChild(commentText);
+    commentCard.appendChild(deleteButton);
 
-        commentsList.appendChild(commentCard);
-    }
+    commentsList.appendChild(commentCard);
+  }
 };
 
+const comentarios_json = localStorage.getItem("comentarios");
+if (comentarios_json) {
+  comentarios = JSON.parse(comentarios_json);
+}
 renderComments(comentarios);
